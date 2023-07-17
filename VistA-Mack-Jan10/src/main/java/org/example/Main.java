96b5446d5749b0650b20ac3a19bb471a514df5ad
abumahfuz1 <46849770+abumahfuz1@users.noreply.github.com>
2023-07-16 21:25:55 -0400

    Add files via upload

---
+++ VistA-Mack-Jan10/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Testing IntelliJ to GitHub, Jan25,2023");
+    }
+}
